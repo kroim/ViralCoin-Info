56eda90b1f2d9d503151411cfba305423eabc109
kroim <wonderfulplayer@outlook.com>
2022-01-05 01:22:09 +0800

    Update Fantom Info page

--- info-fantom/src/utils/index.js
+++ info-fantom/src/utils/index.js
@@ -303,10 +303,10 @@ export const setThemeColor = (theme) => document.documentElement.style.setProper
 export const Big = (number) => new BigNumber(number)
 
 export const urls = {
-  showTransaction: (tx) => `https://etherscan.io/tx/${tx}/`,
-  showAddress: (address) => `https://www.etherscan.io/address/${address}/`,
-  showToken: (address) => `https://www.etherscan.io/token/${address}/`,
-  showBlock: (block) => `https://etherscan.io/block/${block}/`,
+  showTransaction: (tx) => `https://ftmscan.com/tx/${tx}/`,
+  showAddress: (address) => `https://ftmscan.com/address/${address}/`,
+  showToken: (address) => `https://ftmscan.com/token/${address}/`,
+  showBlock: (block) => `https://ftmscan.com/block/${block}/`,
 }
 
 export const formatTime = (unix) => {
